b5a86b8cc6e682688d29c8b28d416b538d2a4c9a
Silverdragon977 <55039285+Silverdragon977@users.noreply.github.com>
2025-08-12 05:08:19 -0500

    Bug Fixes, PHP to Blade, and Added Public Projects table and started the Comments table in Admin Panel

--- resources/views/protectedWebPages/indexAdmin.blade.php
+++ resources/views/protectedWebPages/indexAdmin.blade.php
@@ -22,14 +22,15 @@
                     </div>
                 @endif
             </div>
+            <h2>Projects</h2>
                 <a href="{{ route('admin.createProject') }}"><button type="button">Create Project</button></a>
-            <div class="table-responsive">
+        <!----  Projects Section  ----->  
+            <div id="ProjectsTableAdmin" class="table-responsive">
                 <table class="table table-hover table-dark table-striped table-sm w-100">
                     <tr>
                         <th>Title</th>
                         <th>Languages</th>
                         <th>Github Link</th>
-                        <th>Description</th>
                         <th>Summary</th>
                         <th>Edit</th>
                         <th>Delete</th>
@@ -53,6 +54,41 @@
                     @endforeach
                 </table>
             </div>
+        <!----------------------------->
+        <!----  Comments Section  ----->
+        <br>
+        <h2>Comments</h2>
+            <div id="CommentsTableAdmins" class="table-responsive">
+                <table class="table table-hover table-dark table-striped table-sm w-100">
+                    <tr>
+                        <th>Comment</th>
+                    <!-- <th>Email</th> These will come from users table     -->
+                    <!-- <th>Name</th>                                       -->
+
+                    </tr>
+                    <!-- @foreach($projects as $project)
+                        <tr>
+                            <td>{{ $project->title }}</td>
+                            <td>{{ $project->languages }}</td>
+                            <td><a href="{{ $project->github_link }}"><button type="button">View on Github</button></a></td>
+                            <!-- <td>{{ $project->full_description }}</td> ## Too long and is a README available on github --><!--
+                            <td>{{ $project->short_description }}</td>
+                            <td><a href="{{ route('admin.editProject', ['project' => $project]) }}"><button type="button">Edit</button></a></td>
+                            <td>
+                                <form method="post" action="{{ route('admin.deleteProject', ['project' => $project]) }}">
+                                    @csrf
+                                    @method('delete')
+                                    <input type="submit" value="Delete">
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach -->
+                </table>
+            </div>
+        <!----------------------------->
+
+
+
         </div>
         <div id="commentsSection">
 
--- resources/views/webpages/projects.blade.php
+++ resources/views/webpages/projects.blade.php
@@ -6,4 +6,24 @@
         
     @section('mainContent')
         <h1>Projects</h1>
+        <div id="ProjectsTablePublic"  class="table-responsive">
+                <table class="table table-hover table-dark table-striped table-sm w-100">
+                    <tr>
+                        <th>Title</th>
+                        <th>Languages</th>
+                        <th>Github Link</th>
+                        <th>Summary</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                    @foreach($projects as $project)
+                        <tr>
+                            <td>{{ $project->title }}</td>
+                            <td>{{ $project->languages }}</td>
+                            <td><a href="{{ $project->github_link }}"><button type="button">View on Github</button></a></td>
+                            <!-- <td>{{ $project->full_description }}</td> ## Too long and is a README available on github -->
+                            <td>{{ $project->short_description }}</td>
+                        </tr>
+                    @endforeach
+                </table>
     @endsection
