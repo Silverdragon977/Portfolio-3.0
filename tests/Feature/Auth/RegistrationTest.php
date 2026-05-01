f53f365cc6b5c7606813152e1f70a88fe2f0a4ef
Michael Howard <silverdragon9.8.13@gmail.com>
2026-04-30 20:15:55 -0500

    Modifyed the Auth Registration Test to confirm that after user registered it sends verification and loads the verification.notice blade instead of the /dashboard

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -15,5 +15,6 @@ test('new users can register', function () {
     ]);
 
     $this->assertAuthenticated();
-    $response->assertRedirect(route('dashboard', absolute: false));
+    // $response->assertRedirect(route('dashboard', absolute: false));  Used before email verification
+    $response->assertRedirect(route('verification.notice'));
 });
