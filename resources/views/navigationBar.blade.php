74f0eae7c0a7e0433abc307cda7b193853356bfc
Michael Howard <silverdragon9.8.13@gmail.com>
2026-05-03 03:19:16 -0500

    If user != admin then admin page not clickable now on navigation

--- resources/views/navigationBar.blade.php
+++ resources/views/navigationBar.blade.php
@@ -21,9 +21,11 @@
                 <li><a class="dropdown-item" href="{{ route('projects.index') }}">Projects</a></li>
                 <li><a class="dropdown-item" href="{{ route('resume') }}">Resume</a></li>
                 <li><a class="dropdown-item" href="{{ route('contact.create') }}">Contact</a></li>
-                <li><a class="dropdown-item" href="{{ route('admin.dashboard') }}">Admin</a></li>
-
-
+                @auth
+                    @if(auth()->user()->role === 'admin')
+                        <li><a class="dropdown-item" href="{{ route('admin.dashboard') }}">Admin</a></li>
+                    @endif
+                @endauth
             </ul>
           </li>
           <li class="nav-item dropdown me-1">
